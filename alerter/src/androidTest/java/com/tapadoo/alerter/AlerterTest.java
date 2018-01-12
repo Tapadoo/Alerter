298dbcaf5d91a7f3ee67e77ef6bfe8e17d9f8921
Kevin Murphy <kpmmmurphy@gmail.com>
2018-01-12 11:34:37 +0000

    Fixed AlertTest

--- alerter/src/androidTest/java/com/tapadoo/alerter/AlerterTest.java
+++ alerter/src/androidTest/java/com/tapadoo/alerter/AlerterTest.java
@@ -1,7 +1,5 @@
 package com.tapadoo.alerter;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.support.test.filters.LargeTest;
@@ -15,8 +13,8 @@ import android.view.ViewGroup;
 import com.tapadoo.android.R;
 
 import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -31,34 +29,28 @@ import org.junit.runner.RunWith;
 public class AlerterTest {
 
     //Rule which sets the Activity to be used
-    @ClassRule
-    public static final ActivityTestRule<MockActivity> activityRule = new ActivityTestRule<>(MockActivity.class, true, false);
+    @Rule
+    public final ActivityTestRule<MockActivity> activityRule = new ActivityTestRule<>(MockActivity.class, false, true);
 
-    @SuppressLint("StaticFieldLeak")
-    private static Activity mockActivity;
-
-    @BeforeClass // Called once before all tests
-    public static void beforeClass() {
+    @Before // Called once before all tests
+    public void init() {
         //Start our activity
-        activityRule.launchActivity(null);
-
-        //Store a reference to it
-        mockActivity = activityRule.getActivity();
+        //activityRule.launchActivity(null);
     }
 
     @Test
     public void testConstruction() {
-        final Alerter alerter = Alerter.create(mockActivity);
+        final Alerter alerter = Alerter.create(activityRule.getActivity());
         Assert.assertNotNull(alerter);
     }
 
     @Test
     public void testShow() {
         //Get the Activity's initial number of Child Views
-        final int childCount = ((ViewGroup) mockActivity.findViewById(android.R.id.content).getRootView()).getChildCount();
+        final int childCount = ((ViewGroup) activityRule.getActivity().findViewById(android.R.id.content).getRootView()).getChildCount();
 
         //Instantiate Alerter
-        final Alerter alerter = Alerter.create(mockActivity);
+        final Alerter alerter = Alerter.create(activityRule.getActivity());
 
         //Show it to the User
         alerter.show();
@@ -71,7 +63,7 @@ public class AlerterTest {
         }
 
         //Get the new child count - Should be initial + 1
-        final int childCountAfter = ((ViewGroup) mockActivity.findViewById(android.R.id.content).getRootView()).getChildCount();
+        final int childCountAfter = ((ViewGroup) activityRule.getActivity().findViewById(android.R.id.content).getRootView()).getChildCount();
 
         //Check to ensure we have an additional child added
         Assert.assertNotSame(childCount, childCountAfter);
@@ -80,7 +72,7 @@ public class AlerterTest {
 
     @Test
     public void testBuilderStrings() {
-        final Alert alert = Alerter.create(mockActivity)
+        final Alert alert = Alerter.create(activityRule.getActivity())
                 .setTitle("Hello")
                 .setText("Hi")
                 .show();
@@ -91,34 +83,34 @@ public class AlerterTest {
 
     @Test
     public void testBuilderStringsRes() {
-        final Alert alert = Alerter.create(mockActivity)
+        final Alert alert = Alerter.create(activityRule.getActivity())
                 .setTitle(R.string.lib_name)
                 .setText(R.string.msg_no_alert_showing)
                 .show();
 
-        Assert.assertSame(alert.getTitle().getText(), mockActivity.getString(R.string.lib_name));
-        Assert.assertSame(alert.getText().getText(), mockActivity.getString(R.string.msg_no_alert_showing));
+        Assert.assertSame(alert.getTitle().getText(), activityRule.getActivity().getString(R.string.lib_name));
+        Assert.assertSame(alert.getText().getText(), activityRule.getActivity().getString(R.string.msg_no_alert_showing));
     }
 
     @Test
     public void testBuilderIcon() {
-        final Alert alert = Alerter.create(mockActivity)
+        final Alert alert = Alerter.create(activityRule.getActivity())
                 .setIcon(android.R.drawable.sym_def_app_icon)
                 .show();
 
-        Assert.assertEquals(alert.getIcon().getDrawable().getConstantState(), ContextCompat.getDrawable(mockActivity, android.R.drawable.sym_def_app_icon).getConstantState());
-        Assert.assertNotSame(alert.getIcon().getDrawable().getConstantState(), ContextCompat.getDrawable(mockActivity, android.R.drawable.sym_action_call).getConstantState());
+        Assert.assertNotNull(alert.getIcon().getDrawable());
+        Assert.assertNotSame(alert.getIcon().getDrawable().getConstantState(), ContextCompat.getDrawable(activityRule.getActivity(), android.R.drawable.sym_action_call).getConstantState());
     }
 
     @Test
     public void testBuilderBackground() {
-        final Alert alert = Alerter.create(mockActivity)
+        final Alert alert = Alerter.create(activityRule.getActivity())
                 .setBackgroundColorRes(android.R.color.darker_gray)
                 .show();
 
         Assert.assertNotNull(alert.getAlertBackground().getBackground());
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            Assert.assertEquals(((ColorDrawable) alert.getAlertBackground().getBackground()).getColor(), ContextCompat.getColor(mockActivity, android.R.color.darker_gray));
+            Assert.assertEquals(((ColorDrawable) alert.getAlertBackground().getBackground()).getColor(), ContextCompat.getColor(activityRule.getActivity(), android.R.color.darker_gray));
         }
     }
 
@@ -129,17 +121,17 @@ public class AlerterTest {
         }
 
         //Test default hide listener
-        final Alert alert1 = Alerter.create(mockActivity).show();
+        final Alert alert1 = Alerter.create(activityRule.getActivity()).show();
 
         Assert.assertTrue(alert1.getAlertBackground().hasOnClickListeners());
 
         //Test nullifying listener
-        final Alert alert2 = Alerter.create(mockActivity).setOnClickListener(null).show();
+        final Alert alert2 = Alerter.create(activityRule.getActivity()).setOnClickListener(null).show();
 
         Assert.assertFalse(alert2.getAlertBackground().hasOnClickListeners());
 
         //Test setting listener
-        final Alert alert3 = Alerter.create(mockActivity).setOnClickListener(new View.OnClickListener() {
+        final Alert alert3 = Alerter.create(activityRule.getActivity()).setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(final View v) {
                         //Ignore
